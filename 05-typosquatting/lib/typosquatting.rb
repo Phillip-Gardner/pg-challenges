22f6d100fad6d57b3f0bf8b5e536f4272501d67d
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-06 21:18:55 +0100

    added some comments

--- 05-typosquatting/lib/typosquatting.rb
+++ 05-typosquatting/lib/typosquatting.rb
@@ -1,3 +1,8 @@
 def typosquat(domains, new_domains)
-
+  # split all the domains
+  # check each of the new domains
+  # see if the domain is the same
+  # if it's the same, check if the host is the same
+  # if it's not, push the domain to a typosquat array
+  # return the typosquat array
 end
