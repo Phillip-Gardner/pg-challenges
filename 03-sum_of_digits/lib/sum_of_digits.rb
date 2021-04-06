a13c26f4798b4010355ca23b5bf26b34f004362c
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-06 15:56:33 +0100

    refactor method into 1 line

--- 03-sum_of_digits/lib/sum_of_digits.rb
+++ 03-sum_of_digits/lib/sum_of_digits.rb
@@ -2,10 +2,5 @@
 
 def sum_of_digits(number)
   # initialise sum
-  sum = 0
-  # convert a range into array, iterate over each item before adding to sum
-  (1..number).to_a.each do |element|
-    sum += element.digits.sum
-  end
-  sum
+  (1..number).sum { |i| i.digits.sum }
 end
