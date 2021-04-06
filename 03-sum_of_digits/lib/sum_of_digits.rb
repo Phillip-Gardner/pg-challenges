3c4bcb17caf2a525658fae2df796c5fb7a1299e6
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-06 15:44:41 +0100

    added test cases

---
+++ 03-sum_of_digits/lib/sum_of_digits.rb
@@ -0,0 +1,3 @@
+def sum_of_digits(number)
+  1
+end
--- 03-sum_of_digits/spec/sum_of_digits_spec.rb
+++ 03-sum_of_digits/spec/sum_of_digits_spec.rb
@@ -5,4 +5,12 @@ describe '#sum_of_digits' do
   it 'should return an integer' do
     expect(sum_of_digits(3)).to be_a(Integer)
   end
+  it 'should work for numbers between 1 and 9' do
+    expect(sum_of_digits(1)).to eq(1)
+    expect(sum_of_digits(2)).to eq(3)
+    expect(sum_of_digits(3)).to eq(6)
+    expect(sum_of_digits(5)).to eq(15)
+    expect(sum_of_digits(7)).to eq(28)
+    expect(sum_of_digits(9)).to eq(45)
+  end
 end
