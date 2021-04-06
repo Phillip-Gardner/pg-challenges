d31cf9f2ecae6e9b3606e3f62ed065950051a96b
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-06 21:36:33 +0100

    solved problem

--- 05-typosquatting/lib/typosquatting.rb
+++ 05-typosquatting/lib/typosquatting.rb
@@ -1,8 +1,23 @@
+# frozen_string_literal: true
+
 def typosquat(domains, new_domains)
   # split all the domains
+  old_domains = {}
+  typosquat = []
+  domains.each do |domain|
+    split = domain.split('.')
+    old_domains[split[0]] = split[1]
+  end
   # check each of the new domains
-  # see if the domain is the same
-  # if it's the same, check if the host is the same
-  # if it's not, push the domain to a typosquat array
+  new_domains.each do |domain|
+    split = domain.split('.')
+    # see if the domain is the same
+    next unless old_domains.key?(split[0]) && (old_domains[split[0]] != split[1])
+
+    # if it's the same, check if the host is the same
+    # if it's not, push the domain to a typosquat array
+    typosquat << domain
+  end
   # return the typosquat array
+  typosquat
 end
--- 05-typosquatting/spec/typosquatting_spec.rb
+++ 05-typosquatting/spec/typosquatting_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require_relative '../lib/typosquatting'
 require 'rspec'
 
 domains = ['apple.com', 'google.com', 'tesla.com']
 new_domains = ['apple.com', 'apple.xyz', 'google.org', 'tesla.sla', 'tesla.com', 'yahoo.org']
 
-describe "#typosquatting" do
+describe '#typosquatting' do
   it 'should return the correct domains' do
     expect(typosquat(domains, new_domains)).to eq ['apple.xyz', 'google.org', 'tesla.sla']
   end
