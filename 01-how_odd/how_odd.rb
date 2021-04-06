adf66e406e2c4000fe974e4bbee0cb7f98e34a3b
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-06 12:01:58 +0100

    corrected and ran Rubocop

--- 01-how_odd/how_odd.rb
+++ 01-how_odd/how_odd.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def how_odd(array)
-  # array.count(&:odd?)
+  array.count(&:odd?)
 end
--- 01-how_odd/how_odd_spec.rb
+++ 01-how_odd/how_odd_spec.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'how_odd'
+require 'rspec'
 
 describe 'how_odd' do
-  it "should return an integer" do
+  it 'should return an integer' do
     expect(how_odd([1, 2, 3, 4])).to be_a(Integer)
   end
 
-  it "should give 2 when given [1,2,3]" do
+  it 'should give 2 when given [1,2,3]' do
     expect(how_odd([1, 2, 3])).to eq(2)
   end
 
-  it "should return 0 when given no odd" do
+  it 'should return 0 when given no odd' do
     expect(how_odd([0, 0, 0])).to eq(0)
   end
 end
