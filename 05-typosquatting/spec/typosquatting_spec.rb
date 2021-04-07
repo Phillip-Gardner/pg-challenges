7fa36f106b6165032e7a865f3cb438903bad01fa
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-07 10:41:21 +0100

    commit

--- 05-typosquatting/spec/typosquatting_spec.rb
+++ 05-typosquatting/spec/typosquatting_spec.rb
@@ -4,10 +4,15 @@ require_relative '../lib/typosquatting'
 require 'rspec'
 
 domains = ['apple.com', 'google.com', 'tesla.com']
-new_domains = ['apple.com', 'apple.xyz', 'google.org', 'tesla.sla', 'tesla.com', 'yahoo.org']
+new_domains_one = ['apple.com', 'apple.xyz', 'google.org', 'tesla.sla', 'tesla.com', 'yahoo.org']
+new_domains_two = ['palant|r.xyz', 'apple.com', 'app|e.org', 'apple$hoot.org']
 
 describe '#typosquatting' do
-  it 'should return the correct domains' do
-    expect(typosquat(domains, new_domains)).to eq ['apple.xyz', 'google.org', 'tesla.sla']
+  it 'should return the correct typosquatting domains according to host' do
+    expect(typosquat(domains, new_domains_one)).to eq ['apple.xyz', 'google.org', 'tesla.sla']
+  end
+
+  it 'should return the corret typosquatting domains according to changes in the domain' do
+    expect(typosquat(domains, new_domains_two)).to eq ['apple.xyz', 'google.org', 'tesla.sla']
   end
 end
