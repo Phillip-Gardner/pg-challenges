da905514d44d5301551b1ad0f3b9c46bc06ba254
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-08 14:22:19 +0100

    Added extra test conditions

--- 04-bunnies/lib/bunnies.rb
+++ 04-bunnies/lib/bunnies.rb
@@ -1,7 +1,13 @@
 class Bunny
-  attr_accessor :age
+  attr_accessor :age, :size, :dead?
 
   def initialize
     @age = 0
+    @size = 0
+    dead? = false
+  end
+
+  def bunnies
+    0
   end
 end
--- 04-bunnies/spec/bunnies_spec.rb
+++ 04-bunnies/spec/bunnies_spec.rb
@@ -4,11 +4,26 @@ require 'rspec'
 
 describe Bunny do
   let(:bunny) { Bunny.new }
+
   it 'should have an age' do
     expect(bunny).to respond_to :age
     expect(bunny.age).to be_a Integer
   end
   it 'should be 0 when created' do
     expect(bunny.age).to eq 0
+  end
+
+  it 'should have a size' do
+    expect(bunny).to respond_to :size
+    expect(bunny.size).to be_a Integer
+  end
+
+  it 'should have 0 bunnies when 0 years old' do
+    expect(bunny.bunnies).to eq 0
+  end
+
+  it 'should let us check if its dead or alive' do
+    expect(bunny).to respond_to(:dead?)
+    expect(bunny.dead).to eq(false)
   end
 end
