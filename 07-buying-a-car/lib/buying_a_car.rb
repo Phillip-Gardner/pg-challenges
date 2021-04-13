1535778a2b9a3ce49a0d568e38dfb78478aeb6bf
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-13 16:11:50 +0100

    started coding challenge

---
+++ 07-buying-a-car/lib/buying_a_car.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def buying_a_car(price_old, price_new, saving_per_month, percent_loss_by_month)
+  months = 0
+  # see if the price_old is greater than price_new
+  unless price_old > price_new do
+
+  end
+  # check back next month
+  return [months, price_old - price_new]
+  # if it is, return the money left over and the number of months
+end
--- 07-buying-a-car/spec/buying_a_car_spec.rb
+++ 07-buying-a-car/spec/buying_a_car_spec.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
 
 require_relative '../lib/buying_a_car'
 require 'rspec'
 
-Describe "buying a car" do
-  it "should return the correct answer to test 1" do
-    expect buying_a_car(2000, 8000, 1000, 1.5).to eq [6, 766]
+describe 'buying a car' do
+  it 'should return the correct answer to test 1' do
+    expect(buying_a_car(2000, 8000, 1000, 1.5)).to eq [6, 766]
   end
-  it "should return the correct answer when the answer is 0" do
-    expect buying_a_car(12000, 8000, 1000, 1.5).to eq [0, 4000]
+  it 'should return the correct answer when the answer is 0' do
+    expect(buying_a_car(12_000, 8000, 1000, 1.5)).to eq [0, 4000]
   end
 end
