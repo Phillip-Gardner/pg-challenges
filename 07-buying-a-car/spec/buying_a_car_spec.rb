8c9000c403b725f4b6c6391bec81ac51e6004efe
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-13 16:00:26 +0100

    added car challenge

---
+++ 07-buying-a-car/spec/buying_a_car_spec.rb
@@ -0,0 +1,12 @@
+
+require_relative '../lib/buying_a_car'
+require 'rspec'
+
+Describe "buying a car" do
+  it "should return the correct answer to test 1" do
+    expect buying_a_car(2000, 8000, 1000, 1.5).to eq [6, 766]
+  end
+  it "should return the correct answer when the answer is 0" do
+    expect buying_a_car(12000, 8000, 1000, 1.5).to eq [0, 4000]
+  end
+end
