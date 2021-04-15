e4a61253d8b2634eac67de6080b65aa09a0cc708
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-15 12:30:57 +0100

    interview test prep

---
+++ 08-interview_challenge/spec/interview_challenge_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../lib/interview_challenge'
+require 'rspec'
+
+describe 'hello' do
+  it 'should say hello' do
+    expect(hello).to eq 'hello'
+  end
+end
